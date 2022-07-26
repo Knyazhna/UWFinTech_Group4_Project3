d2ea052ae457626f15aaa1af0610eb99af5a7ad8
Knyazhna <98376200+Knyazhna@users.noreply.github.com>
2022-07-25 19:27:52 -0700

    Front-end code updated

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+##########################
 # Library & Module Imports
+##########################
 import streamlit as st
 from pathlib import Path
 import pandas as pd
@@ -11,10 +13,33 @@ from web3 import Web3
 # Web 3 Connection
 ###################
 # Define and connect a new Web3 provider
-#w3 =
+w3 = Web3(Web3.HTTPProvider(os.getenv("WEB3_PROVIDER_URI")))
+
+# Define the load_contract function
+@st.cache(allow_output_mutation=True)
+def load_contract():
+
+    # Load RSVP Contract ABI
+    with open(Path('./contracts/compiled/RSVP_contract_abi.json')) as f:
+        rsvp_abi = json.load(f)
+
+    # Set the contract address (this is the address of the deployed contract)
+    contract_address = os.getenv("SMART_CONTRACT_ADDRESS")
+
+    # Get the contract
+    contract = w3.eth.contract(
+        address = contract_address,
+        abi = rsvp_abi
+    )
+    # Return the contract from the function
+    return contract
+
+# Load the contract
+contract = load_contract()
+
 
 ###########################
-# Streamlit Interface code.
+# Streamlit Interface code
 ###########################
 
 # Cover Image & Titles
