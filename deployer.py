49210750816540e2e1ce3c284af8a2918aa80137
Arthur Lovett <github@arthurlovett.com>
2022-07-27 17:24:17 -0700

    Finished updating deployer
    Added return fuctions and finished up small bits

--- deployer.py
+++ deployer.py
@@ -48,84 +48,90 @@ def deploy_contract(w3, contract_interface):
     return address
 
 
+
 # Initializing w3 connection
 w3 = Web3(Web3.HTTPProvider(os.getenv("WEB3_PROVIDER_URI")))
 
 
 # Adding contract list to generate filepaths
 contractList = ['eventSafeME', 'Nft721ME', 'rewardTokenME', 'rsvpEventME', 'stakingME']
-abiList = []
- 
+abiList = ['safeAbi', 'nft721Abi', 'rewardAbi','rsvpAbi']
 
-## TODO: Define fn
+def deploy(contractList, abiList):
+    # Initializing list
+    interfaceList = []
 
- # Recursive loading function
-for contract in range(len(contractList)):
-    # Compile source file
-    compiled_sol = compile_source_file(contract)
+    # Recursive loading function
+    for contract in range(len(contractList)):
+        # Compile source file
+        compiled_sol = compile_source_file(contract)
 
-    # Fetch contract interface and ID
-    contract_id, contract_interface = compiled_sol.popitem()
+        # Fetch contract interface and ID
+        contract_id, contract_interface = compiled_sol.popitem()
 
-    # Deploy contract
-    address = deploy_contract(w3, contract_interface)
+        # bytecode = contract_interface['bin']
+        # abi = contract_interface['abi']
+
+        # Deploy contract
+        address = deploy_contract(w3, contract_interface)
+
+        # Saving interfaces for export
+        interface = w3.eth.contract(address=address, abi=contract_interface["abi"])
+        interfaceList.append(interface)
+
+        struct = {
+                "CONTRACT_ADDRESS": address,
+                "CONTRACT_ABI": contract_interface['abi'].abi
+            }
+
+        # Adding json file  
+        with open(('./compiled/' + abiList[contract] + '.json'), 'w', encoding='utf-8') as f:
+            json.dump(struct, f, indent=3)
+        
+
+        print(f'Deployed {contract_id} to: {address}\n')
+        # gas_estimate = store_var_contract.functions.setVar(255).estimate_gas()
+        # print(f'Gas estimate to transact with setVar: {gas_estimate}')
+
+        return interfaceList
 
 
-    print(f'Deployed {contract_id} to: {address}\n')
-    
-## contract id = bin
-# contract name address, abi, contract_interface
+
+# def fetch_abi():
+
+#     contractList = [EVT_Token, CreatorERC721, RSVP_Event]
+#     contractAbiList = ['tokenAbi', 'nftAbi', 'rsvpAbi']
+
+#     for i in range(len(contractList)):
+#         struct = {
+#             "CONTRACT_ADDRESS": str(contractList[i][-1]),
+#             "CONTRACT_ABI": contractList[i][-1].abi
+#         }
+
+#         with open(('./app/src/abi/' + contractAbiList[i] + '.json'), 'w', encoding='utf-8') as f:
+#             json.dump(struct, f, indent=3)
 
 
+# # set pre-funded account as sender
+# >>> w3.eth.default_account = w3.eth.accounts[0]
 
+# >>> Greeter = w3.eth.contract(abi=abi, bytecode=bytecode)
 
+# # Submit the transaction that deploys the contract
+# >>> tx_hash = Greeter.constructor().transact()
 
-def fetch_abi():
+# # Wait for the transaction to be mined, and get the transaction receipt
+# >>> tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
 
-    contractList = [EVT_Token, CreatorERC721, RSVP_Event]
-    contractAbiList = ['tokenAbi', 'nftAbi', 'rsvpAbi']
+# >>> greeter = w3.eth.contract(
+# ...     address=tx_receipt.contractAddress,
+# ...     abi=abi
+# ... )
 
+# >>> greeter.functions.greet().call()
+# 'Hello'
 
-    struct = {
-        "CONTRACT_ADDRESS": str(contract[i][-1]),
-        "CONTRACT_ABI": contracts[i][-1].abi
-    }
-
-    with open(("./contracts/compiled/" + contractAbiList[i] + '.json'), 'w', encoding='utf-8') as f:
-        json.dump(struct, f, indent=3)
-
-# retrieve the contract interface
-contract_id, contract_interface = compiled_sol.popitem()
-
-# get bytecode / bin
->>> bytecode = contract_interface['bin']
-
-# get abi
->>> abi = contract_interface['abi']
-
-# web3.py instance
->>> w3 = Web3(Web3.EthereumTesterProvider())
-
-# set pre-funded account as sender
->>> w3.eth.default_account = w3.eth.accounts[0]
-
->>> Greeter = w3.eth.contract(abi=abi, bytecode=bytecode)
-
-# Submit the transaction that deploys the contract
->>> tx_hash = Greeter.constructor().transact()
-
-# Wait for the transaction to be mined, and get the transaction receipt
->>> tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
-
->>> greeter = w3.eth.contract(
-...     address=tx_receipt.contractAddress,
-...     abi=abi
-... )
-
->>> greeter.functions.greet().call()
-'Hello'
-
->>> tx_hash = greeter.functions.setGreeting('Nihao').transact()
->>> tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
->>> greeter.functions.greet().call()
-'Nihao'
+# >>> tx_hash = greeter.functions.setGreeting('Nihao').transact()
+# >>> tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+# >>> greeter.functions.greet().call()
+# 'Nihao'
