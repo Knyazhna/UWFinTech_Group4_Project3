741e56bc8a0de66d9c45dbcf57fe2a3a0fa6ae23
MaureenKC <99139797+MaureenKC@users.noreply.github.com>
2022-08-03 21:46:21 -0700

    Added citation and comments

--- Contracts/Nft721ME.sol
+++ Contracts/Nft721ME.sol
@@ -1,6 +1,10 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+// ERC721 is a standard for representing ownership of non-fungible tokens, that is, where each token is unique.
+// Ownable represents ownership by account that deployed the NFT contract
+// SafeMath revertS a transaction when an operation overflows.
+
 import '@openzeppelin/contracts/access/Ownable.sol';
 import '@openzeppelin/contracts/utils/Counters.sol';
 import '@openzeppelin/contracts/utils/math/SafeMath.sol';
@@ -11,6 +15,7 @@ contract Nft721 is ERC721, Ownable {
     using Counters for Counters.Counter;
 
 // Use counters to keep track of tokens given during the event.
+// When you say using safemath for uint256 means that it will never overflow unless you pass a 255, but it will not because it will be represented within SafeMath as a uint256 .
 
     Counters.Counter private _tokenIds;
 
@@ -34,3 +39,7 @@ contract Nft721 is ERC721, Ownable {
         _setApprovalForAll(creator, _contract, approved);
     }
 }
+
+
+  // CITATION : Nakglom , Saravut (2022 , February 6 ) Github. solidity-avalanche-RSVP-staking-event/contracts/CreatorERC721.sol 
+ //             https://github.com/SRVng/solidity-avalanche-RSVP-staking-event/blob/ec4c5ed57fc6edff239395e8b7dfa2b387e1bd82/contracts/CreatorERC721.sol
