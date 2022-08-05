ebcfa784534a23ad28fd915c77c50e6260933958
marcoberton <99058088+marcoberton@users.noreply.github.com>
2022-08-04 17:16:45 -0700

    added to HTML WIP, cleaned up code, added comments

---
+++ HTML server WIP/Contracts/rsvpEventME.sol
@@ -0,0 +1,137 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.1;
+
+import './rewardTokenME.sol';
+import './eventSafeME.sol';
+import './stakingME.sol';
+import './Nft721ME.sol';
+
+// Idea : Event Staking (Challenge from https://ethhole.com/challenge)
+// Write a program that lets people pay a small amount, RSVP for an event, and if they don’t show up then everyone who did shares in the reward. 
+// A person can create and event, and check people in. If someone RSVPs but doesn't get checked in they lose their staked ETH.
+
+contract RSVP_Event is Staking,EventSafe {
+    using SafeMath for uint256;
+
+    //Predefined value in enum
+    enum EventStatus { Waiting, Ended }
+
+    //Save enum status in variable
+
+    //Launch this event when status is update
+    event LogEvent(string event_name,string description);
+    event LogRSVP(address participant);
+    
+    //state variable
+    string[] internal OngoingEvent;
+    Event_Detail public event_details;
+    EventStatus public event_status;
+    Nft721 public nft721;
+    address public owner;
+    
+    //struct
+    struct Event_Detail {
+        string event_name;
+        uint256 time_start;
+        uint256 time_end;
+        address creator;
+    }
+    
+    //mapping
+    mapping(address => bool) attendance;
+
+    //When run the contract there should not be any ongoing event
+    constructor(RewardToken _reward, Nft721 _nft721, address _owner) Staking(_reward) {
+        event_status = EventStatus.Ended;
+        nft721 = _nft721;
+        owner = _owner;
+    }
+    
+    function RSVP_Create(string memory event_name, uint256 time_end, uint256 _stake) public payable {
+        //Pay an ether
+        require(OngoingEvent.length == 0, "RSVP event can occur only one at a time");
+        depositor[msg.sender] = true;
+        deposit();
+        
+        //Create an event
+        event_details = Event_Detail(event_name,block.timestamp,time_end, msg.sender);
+        event_status = EventStatus.Waiting;
+        OngoingEvent.push(event_name);
+
+        //NFT Contract
+        getNFTfromContract();
+        giveCreatorNFT(msg.sender);
+        
+        //Stake
+        RSVP(_stake);
+        emit LogEvent(event_name,"RSVP Event Created");
+    }
+
+    function getNFTfromContract() private {
+        nft721.setApprovalForContract(address(this), true);
+        nft721.transferFrom(address(nft721), address(this), 0);
+    }
+
+    function giveCreatorNFT(address creator) private {
+        nft721.transferFrom(address(this), creator, 0);
+        nft721.setApprovalForCreator(tx.origin, address(this), true);
+    }
+    
+    function ongoing_event() public view returns(string memory event_name, uint256 start_from, uint256 until, address creator) {
+        return (event_details.event_name, event_details.time_start, event_details.time_end, event_details.creator);
+    }
+
+    function event_creator() public view returns(address) {
+        return event_details.creator;
+    }
+    
+    function end_time() external view returns(uint256 until) {
+        return event_details.time_end;
+    }
+    
+    function RSVP(uint256 _stake) public {
+        require(event_status == EventStatus.Waiting, "There's no ongoing event");
+        require(block.timestamp < event_details.time_end || nft721.balanceOf(msg.sender) == 1, "It's check in period");
+        stake_once[msg.sender] = true;
+        
+        deposit_stake(_stake);
+
+        attendance[msg.sender] = false;
+        emit LogRSVP(msg.sender);
+    }
+    
+    function Check_in() public {
+        require(block.timestamp > event_details.time_end  && 
+                block.timestamp <= (event_details.time_end + checked_in_period), 
+                "The check-in period didn't start or you missed it, please check the event detail");
+        require(attendance[msg.sender] == false, "You already check-in");
+        
+        withdraw_stake();
+        attendance[msg.sender] = true;
+    }
+    
+    function RSVP_End(address payable your_address) public returns(uint256 shared_amount) {
+        require(nft721.balanceOf(msg.sender) == 1, "The Creator only");
+        require(block.timestamp > (event_details.time_end + checked_in_period), "Please wait");
+        
+        (uint256 _digits,) = reward_share();
+        return_collateral(your_address);
+        getNFTback(false);
+        
+        delete event_details;
+        event_status = EventStatus.Ended;
+        OngoingEvent.pop();
+        
+        return(_digits);
+    }
+
+    function getNFTback(bool isOwner) private {
+        if (!isOwner) {
+            nft721.transferFrom(tx.origin, address(nft721), 0);
+            nft721.setApprovalForCreator(event_details.creator, address(this), false);
+        } else {
+            nft721.transferFrom(event_details.creator, address(nft721), 0);
+            nft721.setApprovalForCreator(event_details.creator, address(this), false);
+        }
+    }
+}
---
+++ HTML server WIP/deployer2_web.py
@@ -0,0 +1,149 @@
+# Imports
+from importlib_metadata import version
+from web3 import Web3
+from solcx import compile_source
+
+import solcx
+import sys
+import os
+import json
+from dotenv import load_dotenv
+from web3 import Web3
+from web3 import EthereumTesterProvider
+
+# Load .env for URI & Contract Address
+load_dotenv()
+
+# solcx.install_solc(version='0.8.9')
+# solcx.set_solc_version('0.8.9')
+"""
+Establishing W3
+Load accounts
+    Reward Token
+    NFT721
+    RSVP
+        address1
+        address2
+        owner
+    Deploy the rest
+"""
+
+# TODO:
+# Add aacounts
+# Add main
+# Maybe seperate
+# Add buttons
+# Need to set parameters for a few 
+
+
+# Solidity source code 
+def compile_source_file(contract):
+    solcx.install_solc(version='0.8.9')
+    solcx.set_solc_version('0.8.9')
+    with open("./Contracts/" + contract + '.sol') as f:
+        source = f.read()
+        print(source)
+    source = "contract Foo {function bar() public {return; }}"
+    return compile_source(source)
+    
+    #      output_values=['abi', 'bin']
+
+
+# Adding function to deploy the compiled Solidity files
+def deploy_contract(w3, contract_interface):
+    tx_hash = w3.eth.contract(
+        abi=contract_interface['abi'],
+        bytecode=contract_interface['bin']).constructor().transact()
+
+    address = w3.eth.get_transaction_receipt(tx_hash)['contractAddress']
+    return address
+
+
+# Initializing w3 connection
+w3 = Web3(Web3.HTTPProvider(os.getenv("WEB3_PROVIDER_URI")))
+
+
+# Adding contract list to generate filepaths
+# contractList = ['eventSafeME', 'Nft721ME', 'rewardTokenME', 'rsvpEventME', 'stakingME']
+# abiList = ['safeAbi', 'nft721Abi', 'rewardAbi','rsvpAbi']
+
+def deploy():
+    # Initializing list
+    contractList = ['eventSafeME', 'Nft721ME', 'rewardTokenME', 'rsvpEventME', 'stakingME']
+    abiList = ['safeAbi', 'nft721Abi', 'rewardAbi','rsvpAbi']
+    interfaceList = []
+
+    # Recursive loading function
+    for contract in (contractList):
+        # Compile source file
+        compiled_sol = compile_source_file(contract)
+
+        # Fetch contract interface and ID
+        contract_id, contract_interface = compiled_sol.popitem()
+
+        # bytecode = contract_interface['bin']
+        # abi = contract_interface['abi']
+
+        # Deploy contract
+        address = deploy_contract(w3, contract_interface)
+
+        # Saving interfaces for export
+        interface = w3.eth.contract(address=address, abi=contract_interface["abi"])
+        interfaceList.append(interface)
+
+        struct = {
+                "CONTRACT_ADDRESS": address,
+                "CONTRACT_ABI": contract_interface['abi'].abi
+            }
+
+        # Adding json file  
+        with open(('./compiled/' + abiList[contract] + '.json'), 'w', encoding='utf-8') as f:
+            json.dump(struct, f, indent=3)
+        
+
+        print(f'Deployed {contract_id} to: {address}\n')
+        # gas_estimate = store_var_contract.functions.setVar(255).estimate_gas()
+        # print(f'Gas estimate to transact with setVar: {gas_estimate}')
+
+        return interfaceList
+
+
+
+# def fetch_abi():
+
+#     contractList = [EVT_Token, CreatorERC721, RSVP_Event]
+#     contractAbiList = ['tokenAbi', 'nftAbi', 'rsvpAbi']
+
+#     for i in range(len(contractList)):
+#         struct = {
+#             "CONTRACT_ADDRESS": str(contractList[i][-1]),
+#             "CONTRACT_ABI": contractList[i][-1].abi
+#         }
+
+#         with open(('./app/src/abi/' + contractAbiList[i] + '.json'), 'w', encoding='utf-8') as f:
+#             json.dump(struct, f, indent=3)
+
+
+# # set pre-funded account as sender
+# >>> w3.eth.default_account = w3.eth.accounts[0]
+
+# >>> Greeter = w3.eth.contract(abi=abi, bytecode=bytecode)
+
+# # Submit the transaction that deploys the contract
+# >>> tx_hash = Greeter.constructor().transact()
+
+# # Wait for the transaction to be mined, and get the transaction receipt
+# >>> tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+
+# >>> greeter = w3.eth.contract(
+# ...     address=tx_receipt.contractAddress,
+# ...     abi=abi
+# ... )
+
+# >>> greeter.functions.greet().call()
+# 'Hello'
+
+# >>> tx_hash = greeter.functions.setGreeting('Nihao').transact()
+# >>> tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+# >>> greeter.functions.greet().call()
+# 'Nihao'
